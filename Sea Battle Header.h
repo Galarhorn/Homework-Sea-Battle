f0980d66d847e2e6608b1f81065ec2a6ddddfba3
Galahorn <aleks_dragonil@mail.ru>
2022-04-27 12:11:56 +0500

    Реализованы начальное поле,а также два поля для игроков с расположенными кораблями

---
+++ Sea Battle Header.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void BlankPage();
+void StartFieldFirstPlayer();
+void StartFieldSecondPlayer();
---
+++ Sea Battle.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <Windows.h>
+#include "Sea Battle Header.h"
+using namespace std;
+string fp, sp; // fp == первый игрок, sp == второй игрок
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	cout << "\t\tДобро пожаловать в игру морской бой!" << "\n\n";
+	cout << "Введите имя первого игрока: " << "\n\n";
+	cin >> fp;
+	cout << "\nВведите имя второго игрока: " << "\n\n";
+	cin >> sp;
+	cout << "\nПоле игрока " << fp << "\n\n";
+	StartFieldFirstPlayer();
+	cout << "\nПоле игрока " << sp << "\n\n";
+	StartFieldSecondPlayer();
+}
